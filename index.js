7dd320169566e2a927dc6c22af49e8cda604151b
CarlosZiegler <carlos.ziegler@gmail.com>
2019-08-01 17:39:33 +0200

    Variable Link, Path charged

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const puppeteer = require('puppeteer')
 
-const tirar= async()=>{
+const getPDF= async()=>{
+    //open Browser Headless
     const browser= await puppeteer.launch()
+    //Create Page in Browser
     const page = await browser.newPage()
-    await page.goto('https://ibjjf.com/championships/calendar/')
-    //await page.goto('https://medium.com/applab/6-web-app-10de2dd1b6d8')
+    //set Link hier:
+    let link = 'https://ibjjf.com/championships/calendar/'
+    //get HTML of link
+    await page.goto(link)
+    //HTML -> PDF
     await page.pdf({
-        path : './Daten/Datei.pdf',
+        path : './Document/Data.pdf',
         format:'A4'})
     await browser.close()
 }
-tirar()
+getPDF()
