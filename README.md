680ffb4b9573f4819d6531177e5c243d977b7d2f
CarlosZiegler <carlos.ziegler@gmail.com>
2019-08-02 15:42:33 +0200

    Readme.md

---
+++ README.md
@@ -0,0 +1,50 @@
+# Project Title
+exportToPDF with Puppeteer (Browser Headless)
+
+## Description
+A simple NodeJS example for a export HTML content of Website to PDF
+
+
+
+## Prerequisites
+
+* [Git](https://git-scm.com/)
+* [Node](https://nodejs.org)
+
+
+## Repository Clone :
+ ```bash
+ git clone https://github.com/CarlosZiegler/exportToPDF.git
+ 
+ ```
+## Install Dependences
+ ```bash
+  cd exportToPDF
+  npm install
+  
+  ```
+
+
+## Getting Started
+
+## Index.js
+ * Change variable " **link** " for your URL.
+  ```javascript
+  let link = 'https://YourCompletURL'
+   ```
+ 
+## Running
+   ```bash
+  node index.js
+  ```
+ 
+ Case success: a PDF has in Path DOCUMENT saved!
+## Information of Packages:
+
+* [puppeteer](https://www.npmjs.com/package/puppeteer) 
+
+## Contributing
+ Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const puppeteer = require('puppeteer')
 
 const getPDF= async()=>{
-    //open Browser Headless
+
+    // Open Browser Headless
     const browser= await puppeteer.launch()
-    //Create Page in Browser
+
+    // Create Page in Browser
     const page = await browser.newPage()
-    //set Link hier:
-    let link = 'https://ibjjf.com/championships/calendar/'
-    //get HTML of link
+
+    //######################################  Set Link hier  ####################################################
+    let link = 'https://medium.com/dockerbr/principais-comandos-docker-f9b02e6944cd'
+    //###########################################################################################################
+
+    // Get HTML of link
     await page.goto(link)
-    //HTML -> PDF
+
+    //With Links to generate PDF
+    await page.emulateMedia('screen');
+
+    // HTML -> PDF in Path Document saved
     await page.pdf({
         path : './Document/Data.pdf',
         format:'A4'})
+
+    // Close Browser
     await browser.close()
+
+    //Response in console
+    console.log('The homepage : '+link + ' has to PDF exported!!!')
 }
 getPDF()
